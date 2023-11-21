0ff1f945de5b2b74319bea63371f87554b3338ec
Mehdi Benouhoud <mehdi.benouhoud@polymtl.ca>
2023-11-21 15:06:49 -0500

    debut squelette api

--- server/app/controllers/database.controller.ts
+++ server/app/controllers/database.controller.ts
@@ -12,6 +12,36 @@ export class DatabaseController {
 
   public get router(): Router {
     const router: Router = Router();
+
+    router.post("/medecins", (req, rep) => {
+      // const medecin = await this.databaseService.addMedecin(req.body)
+      rep.status(501).json(req.body);
+    })
+
+    router.get("/medecins", (req, rep) => {
+      // const allMedecins = await this.databaseService.getAllMedecins();
+      rep.status(501).json({ error: 'Devs didnt code that one yet sorry'});
+    })
+
+    router.get("/medecins/:id", (req, rep) => {
+      const id = req.params.id;
+      // const medecinWithId = await this.databaseService.getMedecin(id);
+      rep.status(501).json({ error: 'Devs didnt code that one yet sorry', id: id});
+    })
+
+    router.delete("/medecins/:id", (req, rep) => {
+      const id = req.params.id;
+      // this.databaseService.deleteMedecin(id);
+      rep.status(501).json({ error: 'Devs didnt code that one yet sorry', id: id});
+    })
+
+    router.patch("/medecins/:id", (req, rep) => {
+      const id = req.params.id;
+      // this.databaseService.updateMedecin(id, req.body);
+      rep.status(501).json({ body: req.body, id: id});
+    })
     return router;
   }
+
+
 }
